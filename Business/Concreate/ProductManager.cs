05e27959195b9c2c8b80e19df355a30a7a5ed2b6
Emirhan Eren <emirhanneren52@gmail.com>
2021-02-22 21:48:50 +0300

    Add project files.

---
+++ Business/Concreate/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concreate
+{
+    public class ProductManager : IProductService
+    {
+        //Injection
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+       
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            //Koşullar
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity ımplement eden bir class veritabanı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
